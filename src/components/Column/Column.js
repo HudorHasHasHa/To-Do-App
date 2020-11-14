8b16b4cde611e412887b3a3026b93c30f17ad143
Igor Jankowski <jankowskiigor5@gmail.com>
2020-11-14 21:04:56 +0100

    Wykorzystanie_props_15.4

---
+++ src/components/Column/Column.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Column.scss';
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+
+class Column extends React.Component {
+    static propTypes = {
+    }
+    render() {
+        return (
+            <section className={styles.component}>
+                <h3 className={styles.title}>{this.props.titleColumn}</h3>
+            </section>
+        )
+    }
+}
+
+export default Column;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js'
 
 class List extends React.Component {
-  render() {
-    return (
-    <section className={styles.Component}>
-        <Hero/>
-    </section>
-    )
-  }
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node,
+        imageSrc: PropTypes.string.isRequired,
+    }
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    }
+    render() {
+        return (
+        <section className={styles.Component}>
+            <Hero titleText={this.props.title} imageSrc={this.props.imageSrc}/>
+            <div className={styles.description}>
+                {this.props.children}
+            </div>
+            <div className={styles.columns}>
+                <Column titleColumn={"Animals"}/>
+                <Column titleColumn={"Plants"}/>
+                <Column titleColumn={"Minerals"}/>
+            </div>
+        </section>
+        )
+    }
 }
 
 export default List;
