58d31fd12566e3a6dda50741e5848463ebd27c0d
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2020-11-24 18:17:48 +0100

    tutury

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../data/dataStore.js';
 import columnsReducer from './columnsRedux.js';
-import cardsReducer from './cardsRedux';
-import serachStringReducer from './searchStringRedux';
+import cardsReducer from './cardsRedux.js';
+import serachStringReducer from './searchStringRedux.js';
 
 // define initial state and shallow-merge initial data
 const initialState = {
