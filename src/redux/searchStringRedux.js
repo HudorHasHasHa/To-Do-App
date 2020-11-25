a869e224d4f18fc500ca01a89adb10c36c39ef42
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2020-11-25 09:55:34 +0100

    Redux_16.5

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,28 +1,29 @@
-import shortid from 'shortid';
+// import shortid from 'shortid';
 
 // selectors
-export const getSearchString = ({ searchString }) => searchString;
+export const getSearchString =  ({searchString}) => searchString.value;
 export const countVisibleCards = ({ cards, searchString }) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 export const countAllCards = ({ cards }) => cards.length;
+
 
 // action name creator
 const reducerName = 'searchString';
 const createActionName = name => `app/${reducerName}/${name}`;
 
-// actions types
+// action types
 export const CHANGE = createActionName('CHANGE');
 
 // action creators
 export const createActionChange = payload => (
-  { payload: { ...payload, id: shortid.generate() },
+  { payload: payload,
     type: CHANGE }
 );
+
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
     case CHANGE:
-      //return [...statePart, action.payload];
       return action.payload;
     default:
       return statePart;
