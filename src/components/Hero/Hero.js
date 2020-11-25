9c1d83325c65cf66bc5fc9f3f7f96e274132998c
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2020-11-25 10:02:18 +0100

    Redux_lint

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,6 +3,7 @@ import styles from './Hero.scss';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
 
+
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
@@ -11,5 +12,6 @@ const Hero = props => (
 );
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
+  imageSrc: PropTypes.string,
 };
 export default Hero;
